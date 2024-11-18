aed855d6f5817992ed98521e9f2d0d18f72bec83
sumin720 <sthoma11@chicagobooth.edu>
2024-11-18 21:23:14 +0000

    lots of stuff but 55%

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "boards", :action => "index" })
   # Routes for the Post resource:
-
+ 
   # CREATE
   post("/insert_post", { :controller => "posts", :action => "create" })
           
